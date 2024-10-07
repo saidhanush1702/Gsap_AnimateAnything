0b2208d4b7b1706ae7553e0deab28e73f27bd3cb
saidhanush1702 <144342564+saidhanush1702@users.noreply.github.com>
2024-10-08 01:57:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,179 @@
+gsap.from(
+    ".l1",{
+        rotateX:180,
+        y:100,
+        duration:0.5,
+
+    }
+)
+
+gsap.from(
+    ".l2",{
+        opacity:0,
+        rotateY:180,
+        duration:0.5,
+        delay:0.5
+    }
+)
+
+gsap.from(
+    ".l3",{
+        y:-200,
+        duration:0.5,
+        delay:1
+    }
+)
+
+gsap.from(
+    ".l4",{
+        x:-200,
+        duration:0.5,
+        delay:1.5
+    }
+)
+
+gsap.from(
+    ".l5",{
+        y:200,
+        delay:2,
+        duration:0.5
+    }
+)
+
+gsap.from(
+    ".l6",{
+        y:200,
+        delay:2.5,
+        duration:0.5
+    }
+)
+
+gsap.from(
+    ".l7",{
+        y:200,
+        delay:2.7,
+        duration:0.3
+    }
+)
+
+
+gsap.from(
+    ".l8",{
+        y:200,
+        duration:1
+    }
+)
+
+
+gsap.to(
+    ".l8",{
+        y:-600,
+        duration:3,
+        delay:0.5
+    }
+)
+
+gsap.from(
+    ".l8",{
+        y:-400,
+        duration:3,
+        delay:1.5
+    }
+)
+
+gsap.to(
+    ".l8",{
+        y:0,
+        duration:1,
+        delay:3.5
+    }
+)
+
+
+
+
+
+
+
+
+
+
+gsap.from(
+    ".l9",{
+        y:200,
+        duration:1
+    }
+)
+
+
+gsap.to(
+    ".l9",{
+        y:-200,
+        duration:2,
+        delay:0.5
+    }
+)
+
+gsap.to(
+    ".l9",{
+        y:0,
+        duration:3,
+        delay:1.5
+    }
+)
+
+gsap.from(
+    ".l10",{
+        scale:0.2,
+        rotateY:360,
+        opacity:0,
+        duration:1.5,
+        delay:2
+    }
+)
+
+gsap.from(
+    ".l11",{
+        scale:0.2,
+        opacity:0,
+        duration:1,
+        delay:2.5
+    }
+)
+
+
+gsap.from(
+    ".l12",{
+        y:-200,
+        duration:0.5,
+        delay:1
+    }
+)
+
+gsap.from(
+    ".l13",{
+        rotateX:360,
+        duration:1.5,
+        repeat:-1
+    }
+)
+
+
+gsap.from(
+    ".l14",{
+        x:-200,
+        duration:0.5,
+        delay:1
+    }
+)
+
+
+gsap.from(
+    ".l15",{
+
+        rotateY:180,
+        rotateZ:-180,
+        duration:1
+
+    }
+)
